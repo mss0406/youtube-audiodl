a5e02a80ed107477ce728b180bdd1cdbdb75d87d
mss0406 <40352865+mss0406@users.noreply.github.com>
2022-01-02 20:21:01 +0100

    Add files via upload

---
+++ audiomp3/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char URL[100];  //100 was chossen for URL with &list= as they tend to be around 85-90 characters
+    char execcmd[200]= " .\\youtube-dl.exe --ffmpeg-location .\\ffmpeg.exe --no-playlist --embed-thumbnail --audio-format mp3 -x ";    //base command
+    int execStartint=102;
+    printf("Enter URL:");
+    scanf("%100s", URL);
+    for(int i = 0; i<=100; i++){
+       execcmd[execStartint+i]=URL[i];
+    }
+    system(execcmd);
+        return 0;
+}
